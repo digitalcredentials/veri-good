3e5425aa417829f5b64c7172b1400faafe4864a4
James Chartrand <jc.chartrand@gmail.com>
2025-12-22 15:35:30 -0500

    push dist for now to allow Github pages to work

---
+++ dist/bundle.js
@@ -0,0 +1,108 @@
+const styles = new CSSStyleSheet();
+
+styles.replaceSync(`
+  :host {
+    --default-color: green;
+    --default-radius: 6px;
+    --default-depth: 5px;
+
+    display: inline-block;
+    contain: content;
+    color: white;
+    background: var(--color, var(--default-color));
+    border-radius: var(--radius, var(--default-radius));
+    min-width: 325px;
+    text-align: center;
+    box-shadow: 0 0 var(--depth, var(--default-depth)) rgba(0,0,0,.5);
+  }
+
+  .header {
+    margin: 16px 0;
+    position: relative;
+  }
+
+  h3 {
+    font-weight: bold;
+    font-family: sans-serif;
+    letter-spacing: 4px;
+    font-size: 32px;
+    margin: 0;
+    padding: 0;
+  }
+
+  h4 {
+    font-family: sans-serif;
+    font-size: 18px;
+    margin: 0;
+    padding: 0;
+  }
+
+  .body {
+    background: white;
+    color: black;
+    padding: 32px 8px;
+    font-size: 42px;
+    font-family: cursive;
+  }
+
+  .footer {
+    height: 16px;
+    background: var(--color, var(--default-color));
+    border-radius: 0 0 var(--radius, var(--default-radius)) var(--radius, var(--default-radius));
+  }
+`);
+
+
+const render = x => `
+  <div part="header" class="header">
+    <h3 part="greeting">${x.registryList.toUpperCase()}</h3>
+    <h4 part="message">Verifiable Credential verfication</h4>
+  </div>
+
+  <div part="body" class="body">
+    <slot></slot>
+    <textarea placeholder="paste your credential or a url pointing to the credential"></textarea>
+    
+  </div>
+
+  
+
+  <div part="footer" class="footer"></div>
+`;
+
+class VeriGood extends HTMLElement {
+
+    static get observedAttributes() {
+    return ['registry-list'];
+  }
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.adoptedStyleSheets = [
+    ...this.shadowRoot.adoptedStyleSheets,
+    styles
+  ];
+    }
+
+    get registryList() {
+    return this.getAttribute('registry-list');
+  }
+
+  set registryList(value) {
+    this.setAttribute('registry-list', value);
+  }
+
+  connectedCallback() {
+    if (!this.registryList) {
+      this.registryList = 'https://digitalcredentials.github.io/dcc-known-registries/known-did-registries.json';
+    }
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.shadowRoot.innerHTML = render(this);
+  }
+
+}
+
+customElements.define('veri-good', VeriGood);
