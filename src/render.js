ef4b01f913d00315a62a005a53038ba0b7feda09
James Chartrand <jc.chartrand@gmail.com>
2026-01-09 15:07:47 -0500

    update sub labels

--- src/render.js
+++ src/render.js
@@ -15,11 +15,11 @@ const render = x => `
    <div id="details-container" class="hide-on-reset">
         <div id="holder-name" class="to-clear"></div>
         <slot name="wasAwarded">
-            <div class="cred-label" >was awarded</div>
+            <div class="cred-label" >was awarded a</div>
         </slot>
         <div id="cred-name" class="to-clear"></div>
         <slot name="awardedBy">
-            <div class="cred-label" >by</div>
+            <div class="cred-label" >by the/div>
         </slot>
         <div id="issuer-name" class="to-clear"></div>
         <div id="more-link">More..</div>
