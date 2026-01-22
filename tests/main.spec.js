8a5f4457c503a1fe7b3f0bea13d4d5764cadfca5
James Chartrand <jc.chartrand@gmail.com>
2026-01-22 09:16:39 -0500

    add test for bad status list

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -5,11 +5,21 @@ const expiredAndRevoked = "https://digitalcredentials.github.io/vc-test-fixtures
 const revokedNoExpiry = "https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v1/bothSignatureTypes/didWeb/legacy-revokedStaus-noExpiry.json"
 const validStatusNoExpiry = "https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v1/ed25519/didWeb/legacy-validStatus-noExpiry.json"
 const badDIDWeb = "https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didWeb/badDidWeb.json"
+const tamperedStatus = "https://digitalcredentials.github.io/vc-test-fixtures/verifiableCredentials/v2/ed25519/didKey/legacy-tamperedStatus-noExpiry.json";
 
 const baseUrl = process.env.BASE_URL ?? 'http://localhost:8080';
 
 test.describe('correct messages show for', () => {
   
+  test('tampered status', async ({ page }) => {
+    await page.goto(`${baseUrl}`);
+    await page.locator('#vc-paste').fill(tamperedStatus);
+    await page.getByRole('button', { name: 'Verify' }).click();
+    await expect(page.getByText('Status unavailable.')).toBeVisible()
+    await expect(page.getByText('No expiry date.')).toBeVisible()
+    await expect(page.getByText('Signature is valid.')).toBeVisible()
+  });
+
   test('expired, no status', async ({ page }) => {
     await page.goto(`${baseUrl}`);
     await page.locator('#vc-paste').fill(expiredNoStatus);
